3c78efa73f13453f4ffd70654aa44d7c77299998
Hugh Cole-Baker <sigmaris@gmail.com>
2018-04-30 22:15:14 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='snips_skill_wa',
+    version='1.0.0',
+    description='Wolfram Alpha skill for Snips',
+    author='Hugh Cole-Baker',
+    url='https://github.com/sigmaris/snips-skill-wolframalpha',
+    download_url='',
+    license='MIT',
+    install_requires=['wolframalpha'],
+    test_suite="tests",
+    keywords=['snips', 'wolfram alpha'],
+    py_modules=['snips_wa'],
+    entry_points={
+        'console_scripts': ['snipswolfram=snips_wa:main'],
+    }
+)
---
+++ snips_wa.py
@@ -0,0 +1,73 @@
+import argparse
+import json
+import logging
+import logging.config
+
+import wolframalpha
+from snipslistener import SnipsListener, intent
+
+LOG = logging.getLogger(__name__)
+
+
+class WolframAlphaListener(SnipsListener):
+
+    def __init__(self, app_id, mqtt_host, mqtt_port=1883):
+        super().__init__(mqtt_host, mqtt_port)
+        self.wa_client = wolframalpha.Client(app_id)
+
+    @intent('convertUnits')
+    def convert_units(self, data):
+        if all(slot in data.slots for slot in ('quantity', 'fromUnit', 'toUnit')):
+            query = "convert {} {} to {}".format(
+                data.slots['quantity'].value, data.slots['fromUnit'].value, data.slots['toUnit'].value
+            )
+        else:
+            query = data.input
+        res = self.wa_client.query(query)
+
+        if res.success != "true":
+            LOG.info("Wolfram Alpha couldn't answer %s", data.input, extra={'response': res})
+            data.session_manager.end_session("Sorry, I couldn't answer that.")
+            return
+        else:
+            LOG.debug("Wolfram Alpha success for %s", data.input, extra={'response': res})
+
+        interpretation = None
+        answer = None
+        for pod in res.pods:
+            if pod.title == "Input interpretation" and pod.numsubpods > 0:
+                subpod = next(pod.subpods)
+                interpretation = subpod.plaintext
+            if pod.primary and pod.numsubpods > 0:
+                subpod = next(pod.subpods)
+                answer = subpod.plaintext
+
+        reply = ''
+        if interpretation is not None:
+            LOG.debug('Interpretation of "%s": %s', data.input, interpretation)
+            # reply += "I interpreted that as: {}. The answer is: ".format(interpretation)
+
+        if answer is not None:
+            LOG.debug('Answer to "%s": %s', data.input, answer)
+            reply += answer
+            data.session_manager.end_session(reply)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="Configuration JSON file", default="config.json")
+    args = parser.parse_args()
+    with open(args.config, 'r') as infile:
+        config = json.load(infile)
+        listener_args = {
+            "mqtt_host": config["mqtt_host"],
+            "app_id": config["app_id"],
+        }
+        if 'mqtt_port' in config:
+            listener_args['mqtt_port'] = int(config['mqtt_port'])
+        if 'logging_config' in config:
+            logging.config.dictConfig(config['logging_config'])
+        else:
+            logging.basicConfig(level=logging.INFO)
+        listener = WolframAlphaListener(**listener_args)
+        listener.loop_forever()
