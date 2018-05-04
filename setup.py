c7b7167a29c1d9ac8cd61d1d49c56422dbbdcec7
Hugh Cole-Baker <sigmaris@gmail.com>
2018-05-04 20:33:29 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snips_wa',
-    version='1.0.2',
+    version='1.0.3',
     description='Wolfram Alpha skill for Snips',
     author='Hugh Cole-Baker',
     url='https://github.com/sigmaris/snips-skill-wolframalpha',
